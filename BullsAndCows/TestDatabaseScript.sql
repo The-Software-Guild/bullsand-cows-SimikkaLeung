5fa6c2604fa9b0f84a9458b690cdc800d6fe8c8f
Simikka <simikka.leung@mail.mcgill.ca>
2023-06-02 14:42:13 -0700

    Adding two SQL scripts;

---
+++ BullsAndCows/TestDatabaseScript.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bullsandcowstest;
+CREATE DATABASE bullsandcowstest;
+USE bullsandcowstest;
+CREATE TABLE game (gameId INT PRIMARY KEY DEFAULT 1, answer VARCHAR(4) NOT NULL, isFinished BOOLEAN NOT NULL);
+CREATE TABLE round (roundId INT PRIMARY KEY DEFAULT 1, guess VARCHAR(4) NOT NULL, exactMatch INT NOT NULL, partialMatch INT NOT NULL, sessionTime DATETIME NOT NULL);
+CREATE TABLE games_rounds 
+(gameId INT, 
+roundId INT, 
+PRIMARY KEY (gameId, roundId), 
+FOREIGN KEY (gameId) REFERENCES game (gameId),
+FOREIGN KEY (roundId) REFERENCES round (roundId)
+);
+
+SELECT * FROM game;
+SELECT * FROM round;
+SELECT * FROM games_rounds;
